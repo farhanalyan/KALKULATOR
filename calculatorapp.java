1e94e878c8721b7ab4e442758d56871d5ba7bff6
farhanalyan <98469206+farhanalyan@users.noreply.github.com>
2023-06-06 22:54:58 +0700

    Create calculatorapp.java

---
+++ calculatorapp.java
@@ -0,0 +1,131 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorApp extends JFrame {
+    private JTextField textField;
+
+    public CalculatorApp() {
+        setTitle("Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLocationRelativeTo(null);
+
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(250, 30));
+        add(textField, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(4, 4));
+        add(buttonPanel, BorderLayout.CENTER);
+
+        String[] buttons = {
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "0", ".", "=", "+"
+        };
+
+        for (String button : buttons) {
+            JButton btn = new JButton(button);
+            btn.addActionListener(new ButtonClickListener());
+            buttonPanel.add(btn);
+        }
+
+        setVisible(true);
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            if (command.equals("=")) {
+                evaluateExpression();
+            } else {
+                textField.setText(textField.getText() + command);
+            }
+        }
+
+        private void evaluateExpression() {
+            String expression = textField.getText();
+            try {
+                double result = evaluate(expression);
+                textField.setText(Double.toString(result));
+            } catch (Exception ex) {
+                textField.setText("Error");
+            }
+        }
+
+        private double evaluate(String expression) {
+            return (double) new Object() {
+                int pos = -1, ch;
+
+                void nextChar() {
+                    ch = (++pos < expression.length()) ? expression.charAt(pos) : -1;
+                }
+
+                boolean eat(int charToEat) {
+                    while (ch == ' ') nextChar();
+                    if (ch == charToEat) {
+                        nextChar();
+                        return true;
+                    }
+                    return false;
+                }
+
+                double parse() {
+                    nextChar();
+                    double x = parseExpression();
+                    if (pos < expression.length()) throw new RuntimeException("Unexpected: " + (char) ch);
+                    return x;
+                }
+
+                double parseExpression() {
+                    double x = parseTerm();
+                    for (; ; ) {
+                        if (eat('+')) x += parseTerm();
+                        else if (eat('-')) x -= parseTerm();
+                        else return x;
+                    }
+                }
+
+                double parseTerm() {
+                    double x = parseFactor();
+                    for (; ; ) {
+                        if (eat('*')) x *= parseFactor();
+                        else if (eat('/')) x /= parseFactor();
+                        else return x;
+                    }
+                }
+
+                double parseFactor() {
+                    if (eat('+')) return parseFactor();
+                    if (eat('-')) return -parseFactor();
+
+                    double x;
+                    int startPos = this.pos;
+                    if (eat('(')) {
+                        x = parseExpression();
+                        eat(')');
+                    } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                        while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                        x = Double.parseDouble(expression.substring(startPos, this.pos));
+                    } else {
+                        throw new RuntimeException("Unexpected: " + (char) ch);
+                    }
+
+                    if (eat('^')) x = Math.pow(x, parseFactor());
+
+                    return x;
+                }
+            }.parse();
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new CalculatorApp();
+            }
+        });
+    }
+}
